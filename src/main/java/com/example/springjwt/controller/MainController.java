4eef10618c8c5dbff45d0604700d826e40133d39
ghkddlscks19 <ghkddlscks19@naver.com>
2024-08-11 15:57:23 +0900

    세션 정보 및 CORS 설정

--- src/main/java/com/example/springjwt/controller/MainController.java
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,13 +1,30 @@
 package com.example.springjwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "Main Controller";
+        //세션에 현재 사용자 아이디
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        //세션에 현재 사용자 롤
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller : " + username + " " + role;
     }
 }
