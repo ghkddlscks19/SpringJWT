aba7f8c8f244bf2faf636161ef040c98a1dc5478
ghkddlscks19 <ghkddlscks19@naver.com>
2024-08-14 15:37:00 +0900

    Spring JWT 심화 - Access 토큰 필터

--- src/main/java/com/example/springjwt/jwt/JwtFilter.java
+++ src/main/java/com/example/springjwt/jwt/JwtFilter.java
@@ -3,16 +3,19 @@ package com.example.springjwt.jwt;
 
 import com.example.springjwt.dto.CustomUserDetails;
 import com.example.springjwt.entity.User;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -24,52 +27,53 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //request에서 Authorization 헤더를 추출
-        String authorization = request.getHeader("Authorization");
 
-        //Authorization 헤더 검증
-        if (authorization == null || !authorization.startsWith("Bearer ")) {
-            System.out.println("token null");
-            //이 필터를 종료하고 다음 필터로 넘겨주기
+        //access 토큰 값을 꺼냄
+        String accessToken = request.getHeader("access");
+
+        //access 토큰 값이 없다면 다음으로 넘김
+        if (accessToken == null) {
             filterChain.doFilter(request, response);
 
-            //조건이 해당되면 메소드 종료(필수)
             return;
         }
 
-        //Bearer 부분 제거 후 순수 토큰 획득
-        String token = authorization.split(" ")[1];
+        //토큰 만료 여부 확인, 만료 시 다음 필터로 넘기지 않음
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
 
-        //토큰 소멸 시간 검증
-        if (jwtUtil.isExpired(token)) {
-            System.out.println("token expired");
-            //이 필터를 종료하고 다음 필터로 넘겨주기
-            filterChain.doFilter(request, response);
-
-            //조건이 해당되면 메소드 종료(필수)
+            //response code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
-        //토큰에서 정보 획득
-        String username = jwtUtil.getUsername(token);
-        String role = jwtUtil.getRole(token);
+        //토큰이 access인지 확인 (발급 시 페이로드에 명시)
+        String category = jwtUtil.getCategory(accessToken);
 
-        //user 엔티티를 생성하여 set
+        if (!category.equals("access")) {
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("invalid access token");
+
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        //username, role 값을 획득
+        String username = jwtUtil.getUsername(accessToken);
+        String role = jwtUtil.getRole(accessToken);
+
         User user = new User();
         user.setUsername(username);
-        //비밀번호는 토큰에 없으나 초기화를 같이 진행해줘야함.
-        //db에서 조회하면 매번 요청할때마다 db를 조회해야하는 손해 발생
-        //ContextHolder에 정확한 비밀번호를 담을 필요가 없으므로 임시 비밀번호 생성
-        user.setPassword("temppassword");
         user.setRole(role);
-
-        //UserDetails에 회원 정보 객체 담기
         CustomUserDetails customUserDetails = new CustomUserDetails(user);
 
-        //스프링 시큐리티 인증 토큰 생성
         Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
-
-        //세션에 사용자 등록
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
